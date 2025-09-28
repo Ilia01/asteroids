b166777331f92f1d99221b7d66c0231cbaa6982e
Ilia01 <ilia.goginashvili.1@btu.edu.ge>
2025-09-28 20:31:41 +0400

    batman

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame
+from constants import *
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    running = True
+    
+    while running:
+        screen.fill(color="black")
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+                return
+
+    
+if __name__ == "__main__":
+    main()
